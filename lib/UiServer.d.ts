0dcbb43b26e5c79649594274f58a1fed5442c266
Pawel Psztyc <jarrodek@gmail.com>
2022-08-18 15:09:58 -0700

    refactor: making some changes
    Signed-off-by: Pawel Psztyc <jarrodek@gmail.com>

---
+++ lib/UiServer.d.ts
@@ -0,0 +1,13 @@
+export const PI = 3.14;
+export const string = 'string';
+export function add (one: number, two: number): number;
+
+export class Console {
+  
+}
+
+type Bla = {
+  add (one: number, two: number): number;
+}
+
+export default Bla;
